63eb5dfa5d64c1229e20d69c017b28e941689860
= <=>
2023-02-02 14:30:35 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
 
+require('dotenv').config() // sử dụng để cấu hình file môi trường
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 configViewEngine(app);
 
