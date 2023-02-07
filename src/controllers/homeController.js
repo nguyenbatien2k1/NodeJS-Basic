3b1c04c91bbd803304f0260e35f9f535a9d5c3d1
= <=>
2023-02-07 15:39:36 +0700

    upload single file

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import pool from "../configs/connectDB";
+import multer from "multer";
+import path from "path";
 
 let getHomepage = async (req, res) => {
     // logic
@@ -40,6 +42,37 @@ let updateUser = async (req, res) => {
   return res.redirect('/')
 }
 
+let uploadFile = async (req, res) => {
+  return res.render('uploadFile.ejs')
+}
+
+const upload = multer().single('profile_pic');
+
+let uploadProfilePic = async (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+  upload(req, res, function(err) {
+    // req.file contains information of uploaded file
+    // req.body contains information of text fields, if there were any
+
+    if (req.fileValidationError) {
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+        return res.send('Please select an image to upload');
+    }
+    else if (err instanceof multer.MulterError) {
+        return res.send(err);
+    }
+    else if (err) {
+        return res.send(err);
+    }
+
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+  });
+
+}
+
 export default {
     getHomepage,
     getDetailsPage,
@@ -47,4 +80,6 @@ export default {
     editUser,
     updateUser,
     deleteUser,
+    uploadFile,
+    uploadProfilePic
 };
