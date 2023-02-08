c35fcf2db28d7feb4612984321718bbfe6b4975a
= <=>
2023-02-08 15:52:33 +0700

    middleware + morgan

--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ import configViewEngine from './configs/viewEngine'
 import initWebRoute from './route/web';
 import initAPIRoute from './route/api';
 
+import morgan from 'morgan';
+
 require('dotenv').config() // sử dụng để cấu hình file môi trường
 
 const app = express()
 const port = process.env.PORT || 3000;
 
+// use morgan
+app.use(morgan('combined'))
 
+// middleware xử lý trong req.body
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
@@ -17,6 +22,11 @@ configViewEngine(app);
 
 // init WEB Route
 initWebRoute(app);
+
+// Handle 404 Not Found
+app.use((req, res) => {
+  return res.render('404.ejs');
+})
 
 // init API Route
 initAPIRoute(app);
